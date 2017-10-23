5ab7590b45e1c70c5a2567214b1469b51c1d2fed
Matheus Moura <matheus.moura@prodeaf.net>
2017-10-23 01:19:04 -0300

    feat: Delegate onStart sendo chamado ao carregar a pagina

--- js/game-engine.js
+++ js/game-engine.js
@@ -1,4 +1,10 @@
 function Game(){
-	var onStart;
-	var onUpdate;
+	var onStart  = function(){};
+	var onUpdate = function(){};
+
+
+	this.init=function(){
+		//quando carregar a pagina invoca a funcao start
+		window.load=this.onStart();
+	};
 }
