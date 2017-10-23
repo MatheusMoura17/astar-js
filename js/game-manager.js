642255d6b447cf8d6853714b78515def5c5a505e
Matheus Moura <matheus.moura@prodeaf.net>
2017-10-23 01:28:40 -0300

    feat: Estrutura do game-manager criada

--- js/game-manager.js
+++ js/game-manager.js
@@ -1,1 +1,16 @@
-alert(o);
+function GameManager(){
+
+	var game=new Game();
+
+	//Quando o jogo começa
+	game.onStart=function(){
+		console.log("game stated");
+	}
+
+	//Frame update
+	game.onUpdate=function(){
+		console.log("game update");
+	}
+
+	game.init();
+}
