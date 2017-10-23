c48c15c89dbf5bcba33b2152aec5b234882e2102
Matheus Moura <matheus.moura@prodeaf.net>
2017-10-23 04:04:49 -0300

    feat: Movendo circulo usando DeltaTime

--- js/game-manager.js
+++ js/game-manager.js
@@ -1,21 +1,24 @@
 function GameManager(){
 
 	this.game;
+	this.counter=0;
 
 	//invoca o inicializador
 	this.init=function(){
+		this.counter=0;
 		this.game=new Game(this.onStart,this.onUpdate);
 		this.game.init();
 	};
 
 	//Quando o jogo começa
 	this.onStart=function(){
-		console.log("gane started");
+		console.log("game started");
 	};
 
 	//Frame update
 	this.onUpdate=function(){
-		console.log("game update");
+		circle=gm.game.elements.circle;
+		circle.cx.baseVal.value += 50*gm.game.deltaTime;
 	};
 
 }
