9507aef0776816c2f040f3a3ecdde077ded10b93
Matheus Moura <matheus.moura@prodeaf.net>
2017-10-23 01:34:46 -0300

    feat: Armazenador de elementos implementado

--- js/game-engine.js
+++ js/game-engine.js
@@ -1,12 +1,17 @@
 function Game(){
 	var onStart  = function(){};
 	var onUpdate = function(){};
+	var elements;
 
 
 	this.init=function(){
 		var game=this;
 		//quando carregar a pagina invoca a funcao start e inicia o update
 		window.load=new function(){
+
+			game.elements=new GameElements();
+			elements.findElements();
+
 			game.onStart();
 			window.setInterval(game.onUpdate,16);
 		};
